a1c4505d95a1a39ff714a619492e66d100e70ab9
Konstantinos <konstantinos@konstantinos-Latitude-E6410.(none)>
2012-02-09 16:57:49 +0100

    check total beacs and total expected and active relation

--- module_unige_wiselib/pltt/PLTT_processor.cpp
+++ module_unige_wiselib/pltt/PLTT_processor.cpp
@@ -155,10 +155,13 @@ namespace wiselib
 		if ( owner().current_time() == 1000 )
 		{
 			NeighborDiscovery::Protocol* p;
-//			wiselib_debug_.debug(" node id %x\n", owner().id() );
-//			p = neighbor_discovery->get_protocol_ref( NeighborDiscovery::TRACKING_PROTOCOL_ID );
-//			p->print( wiselib_debug_, wiselib_radio_ );
-//			wiselib_debug_.debug("-----------\n", owner().id() );
+			wiselib_debug_.debug("----tr-----\n", owner().id() );
+			wiselib_debug_.debug(" node id %x\n", owner().id() );
+			p = neighbor_discovery->get_protocol_ref( NeighborDiscovery::TRACKING_PROTOCOL_ID );
+			p->print( wiselib_debug_, wiselib_radio_ );
+			wiselib_debug_.debug("-----------\n", owner().id() );
+			wiselib_debug_.debug("----vs-----\n", owner().id() );
+			wiselib_debug_.debug("----nb-----\n", owner().id() );
 			wiselib_debug_.debug(" node id %x\n", owner().id() );
 			p = neighbor_discovery->get_protocol_ref( NeighborDiscovery::NB_PROTOCOL_ID );
 			p->print( wiselib_debug_, wiselib_radio_ );
